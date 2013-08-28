fffb69d4f6609bf71da22a04046576c1fdce86b2
dmitry.nester@quadecco.com <dmitry.nester@quadecco.com@2c9b0490-15cc-8f37-6414-10aac4a0d5cc>
2013-08-28 07:49:40 +0000

    +addedproject folder

---
+++ RazerMachine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RazerMachine
+//
+//  Created by Quadecco on 8/23/13.
+//  Copyright (c) 2013 Dima. All rights reserved.
+//
+
+#include <iostream>
+#include "RazerMachineClasses/RazerMachine.h"
+
+int main(int argc, const char * argv[])
+{
+    char path[]= "/Users/kseniiapodoliaka/Documents/cc/RazerMachine/RazerMachine/RazerMachine/testmachine.txt";
+    RazerMachine Machine(path);
+    return 0;
+}
+
