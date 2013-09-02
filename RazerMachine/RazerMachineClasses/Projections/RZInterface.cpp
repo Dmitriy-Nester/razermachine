77414edbe63e412e35ae4c3f70265adae72504b9
dmitriy.nester@gmail.com <dmitriy.nester@gmail.com@2c9b0490-15cc-8f37-6414-10aac4a0d5cc>
2013-09-02 20:31:11 +0000

    +added interface projections Files

---
+++ RazerMachine/RazerMachineClasses/Projections/RZInterface.cpp
@@ -0,0 +1,3 @@
+#include "RZInterface.Hpp"
+
+RZInterface::RZInterface(char name_[]):name(name_){;}
---
+++ RazerMachine/RazerMachineClasses/Projections/RZInterface.hpp
@@ -0,0 +1,14 @@
+#ifndef _RZINTERFACE_HPP_
+#define _RZINTERFACE_HPP_
+#include <vector>
+class RZInterface
+{
+	public:
+	RZInterface(char[]);
+	void* getMethodWithName(const char[]);
+	void addMethod(const char[]);
+	private:
+	std::string name;
+	std::vector<char*> methods;
+};
+#endif
